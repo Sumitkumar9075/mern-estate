9762d6e8a04d2dcba3edc34bae58937d7d800f78
Sumitkumar9075 <sumitkumar18886@gmail.com>
2024-06-12 10:53:10 +0530

    created sign up api route

--- api/controllers/user.controllers.js
+++ api/controllers/user.controllers.js
@@ -1,5 +1,5 @@
 export const test = (req ,res) =>{
     res.json({
-        message : "My name is Sumit Kumar (2003794) and I am a software Engineer."
+        message : "My name is Sumit Kumar (2003794) and I am a software Engineer. "
     })
 }
