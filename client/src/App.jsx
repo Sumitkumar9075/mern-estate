b16caac1a992604a66bde5b6da24362f133bdb30
Sumitkumar9075 <sumitkumar18886@gmail.com>
2024-06-25 20:50:47 +0530

    Completed Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,40 @@
-import { BrowserRouter ,Route ,Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Profile from "./pages/Profile"
-import Header from "./components/Header"
-import PrivateRoute from "./components/PrivateRoute"
-import CreateListing from "./pages/CreateListing"
-import UpdateListing from "./pages/UpdateListing"
-import Listing from "./pages/Listing"
-import Search from "./pages/Search"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import CreateListing from "./pages/CreateListing";
+import UpdateListing from "./pages/UpdateListing";
+import Listing from "./pages/Listing";
+import Search from "./pages/Search";
+import Footer from "./components/Footer";
+import './index.css';  // Import the Tailwind CSS file
 
 export default function App() {
   return (
-  <BrowserRouter>
-  <Header/>
-  <Routes>
-    <Route path="/" element = {<Home/>}></Route>
-    <Route path="/sign-in" element = {<SignIn/>}></Route>
-    <Route path="/sign-up" element = {<SignUp/>}></Route>
-    <Route path="/about" element = {<About/>}></Route>
-    <Route path="/search" element = {<Search/>}></Route>
-
-
-    <Route path="/listing/:listingId" element = {<Listing/>}></Route>
-
-    <Route  element = {<PrivateRoute/>}>
-    <Route path="/profile" element = {<Profile/>}></Route>
-    <Route path="/create-listing" element = {<CreateListing/>}></Route>
-    <Route path="/update-listing/:listingId" element = {<UpdateListing/>}></Route>
-
-
-    </Route>
-
-
-
-  </Routes>
-  </BrowserRouter>
-  )
+    <BrowserRouter>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/listing/:listingId" element={<Listing />} />
+            <Route element={<PrivateRoute />}>
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/create-listing" element={<CreateListing />} />
+              <Route path="/update-listing/:listingId" element={<UpdateListing />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.jsx
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-white text-center p-4 w-full  bottom-0">
+      <p>&copy; 2024 Rentify. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
